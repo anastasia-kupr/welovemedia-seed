7d65452bf3b504b4703e4ad0635dc67e8082aaf9
Anastasia <anastasia.kupr@gmail.com>
2019-04-23 19:06:15 +0300

    Init seed.

---
+++ seed.js
@@ -0,0 +1,76 @@
+const md5 = require('js-md5');
+const fs = require("fs");
+
+const count = 10;
+const filename = `./seeds/subsctibers-${count}.sql`;
+
+var stream = fs.createWriteStream(filename, {flags: 'w'});
+
+module.exports = async () => {
+
+
+  const domains = [
+    "mailer-wld.pl"
+  ]
+
+  stream.write(`INSERT INTO "subscribers"
+  ("mailings",
+  "active",
+  "base_limit",
+  "base_id",
+  "name",
+  "email",
+  "token",
+  "md5",
+  "domain",
+  "lead_active",
+  "gender",
+  "year_of_birth",
+  "city",
+  "province",
+  "created",
+  "attr_str_1")  VALUES\n`);
+  const count2 = 10;
+  for (let i = 0; i < count - 1; i += count2) { //50000000
+    let output = '';
+
+    for (let b = i; b < i + count2; b++) {
+      const base_id = b < i + count2 / 2 ? 1 : 2;
+      const name = randomString(20);
+      const hash = md5(`${name}0${i}@${domains[0]}`);
+      const email = `${name}0${i}@${domains[0]}`;
+      const year_of_birth = Math.floor(Math.random() * 85) + 1933;
+      const lead_active = Math.floor(Math.random() * 10);
+      const gender = !!(Math.floor(Math.random() * 2));
+      const city = Math.floor(Math.random() * 85);
+      const province = Math.floor(Math.random() * 16);
+      output += `('{}',true,10,${base_id},'name_${name}0${i}@${domains[0]}','${email}',decode('${hash}', 'hex'),decode('${hash}', 'hex'),'${domains[0]}','${lead_active}',${gender},${year_of_birth},${city},${province},'2018-12-12 22:34:15','surname_${name}000000000${i}@${domains[0]}'),\n`;
+    }
+
+    stream.write(output);
+  }
+
+  const base_id = 1;
+  const name = randomString(20);
+  const hash = md5(`${name}00@${domains[0]}`);
+  const email = `${name}00@${domains[0]}`;
+  const year_of_birth = Math.floor(Math.random() * 85) + 1933;
+  const lead_active = Math.floor(Math.random() * 10);
+  const gender = !!(Math.floor(Math.random() * 2));
+  const city = Math.floor(Math.random() * 85);
+  const province = Math.floor(Math.random() * 16);
+  const lastOutput = `('{}',true,10,${base_id},'name_${name}00@${domains[0]}','${email}',decode('${hash}', 'hex'),decode('${hash}', 'hex'),'${domains[0]}','${lead_active}',${gender},${year_of_birth},${city},${province},'2018-12-12 22:34:15','surname_${name}0000000000@${domains[0]}');`;
+
+  stream.write(lastOutput);
+
+};
+
+function randomString(len, charSet) {
+  charSet = charSet || 'abcdefghijklmnopqrstuvwxyz';
+  let randomString = '';
+  for (let i = 0; i < len; i++) {
+    let randomPoz = Math.floor(Math.random() * charSet.length);
+    randomString += charSet.substring(randomPoz, randomPoz + 1);
+  }
+  return randomString;
+}
